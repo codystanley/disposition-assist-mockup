91ca65bd15899d6864c780e20273755d912df693
codystanley <cody.stanley@triagelogic.com>
2024-06-17 16:25:29 -0400

    Added docker. Added all green button. No worky.

--- script.js
+++ script.js
@@ -3,15 +3,17 @@
 /* -------- */
 
 const clickDispositionList = document.getElementById("clickDispositionList");
+
 const clickSymptoms = [
     "[1] Coughed up blood AND [2] more than blood-tinged sputum",
     "Retractions - skin between the ribs is pulling in (sinking in) with each breath (includes suprasternal retractions)",
     "Stridor (harsh sound with breathing in) is present",
     "[1] Lips or face have turned bluish BUT [2] only during coughing fits",
     "[1] Age < 12 weeks AND [2] fever 100.4 F (38.0 C) or higher by any route (Note: Preference is to confirm with rectal temperature)"
-]; // Add your symptoms here
+];
 
-// Dynamically create list items
+const toggleButton = document.getElementById("m1AllGreen")
+
 clickSymptoms.forEach(symptom => {
     const listItem = document.createElement("li");
     listItem.textContent = symptom;
@@ -23,17 +25,17 @@ clickSymptoms.forEach(symptom => {
   
     listItem.addEventListener("click", () => {
         if (listItem.classList.contains("btn-warning")) {
-          listItem.classList.remove("btn-warning");
-          listItem.classList.add("btn-success");
+            listItem.classList.remove("btn-warning");
+            listItem.classList.add("btn-success");
         } else if (listItem.classList.contains("btn-success")) {
-          listItem.classList.remove("btn-success"); // Remove green
-          listItem.classList.add("btn-danger");
+            listItem.classList.remove("btn-success"); // Remove green
+            listItem.classList.add("btn-danger");
         } else { // Default (grey)
-          listItem.classList.remove("btn-danger");
-          listItem.classList.add("btn-warning");
+            listItem.classList.remove("btn-danger");
+            listItem.classList.add("btn-warning");
         }
-      });
-  });
+    });
+});
 
 /* -------- */
 /* Method 2 */
